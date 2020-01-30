bc52c74b87857f0df2f96c2e3ec689c03528d03b
Dan Oneata <dan.oneata@gmail.com>
2020-01-30 14:20:15 +0200

    FIX Paths for LRW

--- scripts/lrw/extract_audio_from_video.sh
+++ scripts/lrw/extract_audio_from_video.sh
@@ -3,10 +3,10 @@ extract1 () {
     w=$(echo $path1 | cut -f4 -d/)
     s=$(echo $path1 | cut -f5 -d/)
     n=$(echo $path1 | cut -f6 -d/ | cut -f1 -d.)
-    mkdir -p data/lrw/frames/$w/$s
+    mkdir -p data/lrw/audio-from-video/$w/$s
     ffmpeg -i $path1 -vn -acodec pcm_s16le -ar 16000 -ac 2 data/lrw/audio-from-video/$w/$s/$n.wav
 }
 
 export -f extract1
 
-find data/lrw/video -name '*.mp4' | parallel -j8 extract1 {}
+find data/lrw/video/ -name '*.mp4' | parallel -j8 extract1 {}
