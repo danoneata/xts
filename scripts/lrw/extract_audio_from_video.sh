42fd4925af17a439d885c4989696a1a4822f52a9
Dan Oneata <dan.oneata@gmail.com>
2020-01-30 14:11:43 +0200

    More scripts for LRW

---
+++ scripts/lrw/extract_audio_from_video.sh
@@ -0,0 +1,12 @@
+extract1 () {
+    path1=$1
+    w=$(echo $path1 | cut -f4 -d/)
+    s=$(echo $path1 | cut -f5 -d/)
+    n=$(echo $path1 | cut -f6 -d/ | cut -f1 -d.)
+    mkdir -p data/lrw/frames/$w/$s
+    ffmpeg -i $path1 -vn -acodec pcm_s16le -ar 16000 -ac 2 data/lrw/audio-from-video/$w/$s/$n.wav
+}
+
+export -f extract1
+
+find data/lrw/video -name '*.mp4' | parallel -j8 extract1 {}
