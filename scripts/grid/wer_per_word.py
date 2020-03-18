4538e723bad9b0f97692538b651a7d8de0bc82fa
Dan Oneata <dan.oneata@gmail.com>
2020-03-18 11:14:27 +0200

    FIX The ASR doesn't enforce the grammar
    I have observed that for some of the synthesised utterances (possibly
    the worst generated ones), the decoded sentence is not six words long,
    as it should be. This is not expected since, we had a rigid grammar that
    should have been enforced by the model. However, in order to properly
    calculate the per-word error, I had to ignore some of the utterances.

--- scripts/grid/wer_per_word.py
+++ scripts/grid/wer_per_word.py
@@ -21,7 +21,7 @@ def get_nth(d, n):
 
 
 def wer(d1, d2):
-    keys = d1.keys()
+    keys = d2.keys()
     n_total = len(keys)
     n_correct = sum(d1[k] == d2[k] for k in keys)
     return 1 - n_correct / n_total
@@ -32,5 +32,11 @@ pred = load_data(f"/tmp/pred-{model_name}")
 true = load_data(f"/home/doneata/work/experiments-tedlium-r2/exp_grid/chain_cleaned/tdnn1f_sp_bi/decode-grammar_unseen-k-small-test_synth-{model_name}/scoring/test_filt.txt")
 
 fmt = lambda wer: f"{100 * wer:4.1f}"
+
+print("Filter out shorter predictions:")
+print(len(pred), end=" → ")
+pred = {k: v for k, v in pred.items() if len(v) == 6}
+print(len(pred))
+
 word_wer = (wer(get_nth(true, n), get_nth(pred, n)) for n in range(N_WORDS))
 print(" | ".join(fmt(wer) for wer in word_wer))
